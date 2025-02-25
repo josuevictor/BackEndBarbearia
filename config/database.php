a9014fe7bb9f48d61f7b2dfdfaf834d813c394a5
Josue <victorarroxellas01@gmail.com>
2025-02-25 17:11:42 -0300

    databese

--- config/database.php
+++ config/database.php
@@ -53,10 +53,10 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DB_URL', 'postgresql://agendamento_38eg_user:KViCWCJDWdu1jwpwCHrF71OUDPKYDKcg@dpg-cutn4v8gph6c73b4j1tg-a/agendamento_38eg'),
             'host' => env('DB_HOST', 'dpg-cutn4v8gph6c73b4j1tg-a'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'agendamento'),
+            'database' => env('DB_DATABASE', 'agendamento_38eg'),
             'username' => env('DB_USERNAME', 'agendamento_38eg_user'),
             'password' => env('DB_PASSWORD', 'KViCWCJDWdu1jwpwCHrF71OUDPKYDKcg'),
             'charset' => env('DB_CHARSET', 'utf8'),
