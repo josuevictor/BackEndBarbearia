4c723d2c7fb7ca3a631225494b805c61d00c2a60
Josue <victorarroxellas01@gmail.com>
2025-02-25 17:31:15 -0300

    database

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL', 'postgresql://agendamento_38eg_user:KViCWCJDWdu1jwpwCHrF71OUDPKYDKcg@dpg-cutn4v8gph6c73b4j1tg-a/agendamento_38eg'),
+            'url' => env('DB_URL', 'postgresql://agendamento_38eg_user:KViCWCJDWdu1jwpwCHrF71OUDPKYDKcg@dpg-cutn4v8gph6c73b4j1tg-a.oregon-postgres.render.com/agendamento_38eg'),
             'host' => env('DB_HOST', 'dpg-cutn4v8gph6c73b4j1tg-a'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'agendamento_38eg'),
