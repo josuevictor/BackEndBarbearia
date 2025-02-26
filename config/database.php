44053a323a1e60c672b76dcd69d2eb8e0e872abf
Josue <victorarroxellas01@gmail.com>
2025-02-26 09:25:34 -0300

    alteração no database.php

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
     ],
