877b93223ba591c85b38404ce87eb3900500d314
Josue <victorarroxellas01@gmail.com>
2025-02-26 17:07:40 -0300

    definida a pk na model clientes

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     protected $table = 'agendamento.clientes';
+    protected $primaryKey = 'cliente_id';
     public $timestamps = false;
     use HasFactory;
 }
